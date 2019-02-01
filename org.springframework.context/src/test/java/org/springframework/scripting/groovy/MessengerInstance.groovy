edf05850ceefb2e66dd8c7f3b542cb951abfeabe
za-chujun <1571970439@qq.com>
2019-02-01 10:34:16 +0800

    exist two GroovyMessenger:GroovyMessengerV2

--- org.springframework.context/src/test/java/org/springframework/scripting/groovy/MessengerInstance.groovy
+++ org.springframework.context/src/test/java/org/springframework/scripting/groovy/MessengerInstance.groovy
@@ -2,13 +2,13 @@ package org.springframework.scripting.groovy;
 
 import org.springframework.scripting.Messenger
 
-class GroovyMessenger implements Messenger {
+class GroovyMessengerV2 implements Messenger {
 
-	GroovyMessenger() {
+	GroovyMessengerV2() {
 		println "GroovyMessenger"
 	}
 
 	def String message;
 }
 
-return new GroovyMessenger();
+return new GroovyMessengerV2();
